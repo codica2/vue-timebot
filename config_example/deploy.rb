a0443ba62380ff8f2c1ba00a1b6a2a1caf137d8d
Valentin Yakovlev <vyakovlev.codica@gmail.com>
2018-12-20 15:31:53 +0200

    [Feature] add cofig example

---
+++ config_example/deploy.rb
@@ -0,0 +1,31 @@
+require 'mina/rails'
+require 'mina/git'
+
+set :application_name, 'vue-timebot'
+set :domain, ''
+set :user, 'dev'
+set :deploy_to, "/home/dev/www/#{fetch(:application_name)}"
+set :repository, 'git@gitlab.example.com:example/example.git'
+set :branch, 'master'
+
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  # command %{rbenv install 2.3.0 --skip-existing}
+end
+
+desc "Deploys the current version to the server."
+task :deploy do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:cleanup'
+
+    on :launch do
+      in_path(fetch(:current_path)) do
+        command %{yarn install}
+        command %{yarn build}
+      end
+    end
+  end
+end
+
